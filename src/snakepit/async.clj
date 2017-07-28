1eee4d4c6c21860c621f07460f15fed9ee0f42f6
Paul Gowder <paul.gowder@gmail.com>
2017-07-28 00:51:38 -0400

    wire up async for receiving too

--- src/snakepit/async.clj
+++ src/snakepit/async.clj
@@ -10,9 +10,6 @@
              :refer [>! <! >!! <!! go chan buffer close! thread
                      alts! alts!! timeout go-loop]]))
 
-(def receiver-async-chan (chan))
-(def sender-async-chan (chan))
-
 (defn say [async-channel message]
   (go
     (>! async-channel message)))
@@ -23,17 +20,35 @@
       (lb/publish mq-channel "" queue content {:content-type "text/plain"}))
     (recur)))
 
-(defn message-handler
-  [ch metadata ^bytes payload]
+(defn answer [msg]
   (println "Clojure takes your Python and doubles it. "
-           (mapv #(* 2 %)
-                 (json/read-str (String. payload "UTF-8")))))
+           (mapv #(* 2 %) msg)))
+
+(defn message-receiver
+  [async-channel mq-channel metadata ^bytes payload]
+  (let [message (json/read-str (String. payload "UTF-8"))]
+    (go
+      (>! async-channel message))))
+
+(defn listen! [async-channel mq-channel queue]
+  (let [handler (partial message-receiver async-channel)]
+    (lc/subscribe mq-channel queue handler {:auto-ack true})))
+
+(defn process-messages! [async-channel processor]
+  (go-loop []
+    (let [message (<! async-channel)]
+      (processor message))
+    (recur)))
+;; should this be a plain loop in a thread that blocks, i.e. <!! rather than <! and thread not go?  or should it be go-loop?
 
 (defn async-comm [mq-channel]
-  (let [send! (partial say sender-async-chan)]
+  (let [receiver-async-chan (chan)
+        sender-async-chan (chan)
+        send! (partial say sender-async-chan)]
     (dispatch-messages! mq-channel sender-async-chan)
     (lq/declare mq-channel "jsonpy2clj" {:exclusive false :auto-delete false})
     (lq/declare mq-channel "jsonclj2py" {:exclusive false :auto-delete false})
-    (println (format "Clojure Connected. Channel id: %d" (.getChannelNumber mq-channel)))
-    (lc/subscribe mq-channel "jsonpy2clj" message-handler {:auto-ack true})
+    (println "Clojure Connected.")
+    (listen! receiver-async-chan mq-channel "jsonpy2clj")
+    (process-messages! receiver-async-chan answer)
     (send! {:queue "jsonclj2py" :content (json/write-str [9, 10, 11])})))
